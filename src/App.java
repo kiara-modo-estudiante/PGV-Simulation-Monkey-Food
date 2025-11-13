aa52e3310e0c314f46c01563d169fe64bbcbea92
Kiara Maldonado <usr4066@salesianos-lacuesta.net>
2025-11-13 12:47:37 +0000

    feat(app): initialize SharedResource and create Tourist instances for food management

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import net.salesianos.shared.SharedResource;
+import net.salesianos.threads.Tourist;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        SharedResource sharedResource = new SharedResource(5);
+
+        Tourist.setSharedResource(sharedResource);
+
+        String[] food = { "🍌 Banana", "🍎 Apple", "🍊 Orange", "🍇 Grapes", "🍍 Pineapple", "🥭 Mango", "🍑 Peach",
+                "🍓 Strawberry",
+                "🫐 Blueberry", "🍉 Watermelon" };
+
+        Tourist tourist1 = new Tourist(1, "👩 Alice", 3, food, 3);
+        Tourist tourist2 = new Tourist(2, "👴 Bob", 3, food, 3);
+
+        tourist1.start();
+        tourist2.start();
     }
 }
