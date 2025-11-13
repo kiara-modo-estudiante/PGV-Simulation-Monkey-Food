62c626f89a7a2a28d3bcf09266279a8d919e8d8a
Kiara Maldonado <usr4066@salesianos-lacuesta.net>
2025-11-13 12:20:43 +0000

    refactor(tourist): migrate Tourist class to a thread-based implementation

---
+++ src/net/salesianos/threads/Tourist.java
@@ -0,0 +1,90 @@
+package net.salesianos.threads;
+
+import java.util.Arrays;
+
+public class Tourist extends Thread {
+
+    // ----------- Attributes -----------
+    private int touristId;
+    private String touristName;
+    private int totalQuantityToProduce;
+    private String[] food;
+    private int maxTimeToProduce;
+
+    // ----------- Complete Constructor -----------
+    public Tourist(int touristId, String touristName, int totalQuantityToProduce, String[] food, int maxTimeToProduce) {
+        this.touristId = touristId;
+        this.touristName = touristName;
+        this.totalQuantityToProduce = totalQuantityToProduce;
+        this.food = food;
+        this.maxTimeToProduce = maxTimeToProduce;
+    }
+
+    // ----------- Getters and Setters -----------
+    public int getTouristId() {
+        return touristId;
+    }
+
+    public void setTouristId(int touristId) {
+        this.touristId = touristId;
+    }
+
+    public String getTouristName() {
+        return touristName;
+    }
+
+    public void setTouristName(String touristName) {
+        this.touristName = touristName;
+    }
+
+    public int getTotalQuantityToProduce() {
+        return totalQuantityToProduce;
+    }
+
+    public void setTotalQuantityToProduce(int totalQuantityToProduce) {
+        this.totalQuantityToProduce = totalQuantityToProduce;
+    }
+
+    public String[] getFood() {
+        return food;
+    }
+
+    public void setFood(String[] food) {
+        this.food = food;
+    }
+
+    public int getMaxTimeToProduce() {
+        return maxTimeToProduce;
+    }
+
+    public void setMaxTimeToProduce(int maxTimeToProduce) {
+        this.maxTimeToProduce = maxTimeToProduce;
+    }
+
+    // ----------- Thread Logic -----------
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < totalQuantityToProduce; i++) {
+                // Simulate producing food
+                String producedFood = food[i % food.length];
+                System.out.println("Tourist " + touristName + " produced: " + producedFood);
+
+                // Simulate time taken to produce food
+                int timeToProduce = (int) (Math.random() * maxTimeToProduce) + 1;
+                Thread.sleep(timeToProduce * 1000); // Convert seconds to milliseconds
+            }
+            System.out.println("Tourist " + touristName + " has finished producing food.");
+        } catch (InterruptedException e) {
+            System.err.println("Tourist " + touristName + " was interrupted.");
+        }
+    }
+
+    // ----------- toString -----------
+    @Override
+    public String toString() {
+        return "Tourist [touristId=" + touristId + ", name=" + touristName + ", totalQuantityToProduce="
+                + totalQuantityToProduce
+                + ", food=" + Arrays.toString(food) + ", maxTimeToProduce=" + maxTimeToProduce + "]";
+    }
+}
