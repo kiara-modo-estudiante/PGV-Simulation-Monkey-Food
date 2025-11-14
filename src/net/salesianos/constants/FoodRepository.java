73b6f48f88b656a21d365b9dd1d7f2802b7234e7
Kiara Maldonado <usr4066@salesianos-lacuesta.net>
2025-11-14 12:34:52 +0000

    refactor(foodRepository): instantiate Random locally in getRandomFruit method

--- src/net/salesianos/constants/FoodRepository.java
+++ src/net/salesianos/constants/FoodRepository.java
@@ -9,14 +9,14 @@ public class FoodRepository {
             "(🥝) Kiwi", "(🍒) Cherry", "(🍋) Lemon", "(🍈) Melon", "(🍏) Green Apple"
     };
 
-    private static final Random random = new Random();
-
     /**
      * Selects a random fruit from the food array.
      * 
      * @return A random fruit from the food array.
      */
     public static String getRandomFruit() {
+        Random random = new Random();
+
         return FOOD[random.nextInt(FOOD.length)];
     }
 }
