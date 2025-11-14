52565dec4bd003885ed9acec701ae09134cf2c51
Kiara Maldonado <usr4066@salesianos-lacuesta.net>
2025-11-14 17:03:17 +0000

    refactor(App): add additional tourists and monkeys with updated parameters

--- src/App.java
+++ src/App.java
@@ -11,15 +11,21 @@ public class App {
         System.out.println("╚════════════════════════════════════════════════╝\n" + //
                 "");
 
-        Tourist touristOne = new Tourist(1, "(👩) Alice", 3, 3, monkeyPark);
-        Tourist touristTwo = new Tourist(2, "(👴) Bob", 3, 3, monkeyPark);
+        Tourist touristOne = new Tourist(1, "(👩) Alice", 2, 2, monkeyPark);
+        Tourist touristTwo = new Tourist(2, "(👴) Bob", 3, 2, monkeyPark);
+        Tourist touristThree = new Tourist(3, "(👶) Timmy", 8, 2, monkeyPark);
 
-        Monkey monkeyOne = new Monkey(1, "(🐒) George", 3, 2, monkeyPark);
+        Monkey monkeyOne = new Monkey(1, "(🐒) George", 2, 2, monkeyPark);
         Monkey monkeyTwo = new Monkey(2, "(🐒) Charlie", 3, 4, monkeyPark);
+        Monkey monkeyThree = new Monkey(3, "(🦍) Kong", 7, 2, monkeyPark);
+        Monkey monkeyFour = new Monkey(4, "(🐒) Pepito", 1, 10, monkeyPark);
 
         touristOne.start();
         touristTwo.start();
+        touristThree.start();
         monkeyOne.start();
         monkeyTwo.start();
+        monkeyThree.start();
+        monkeyFour.start();
     }
 }
