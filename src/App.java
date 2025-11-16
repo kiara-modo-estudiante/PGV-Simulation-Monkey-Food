07a29c4d27d1846247ab0c94085a3e9586dac1e4
Kiara Maldonado <usr4066@salesianos-lacuesta.net>
2025-11-16 15:14:32 +0000

    feat(experimentoUno): modify app to comply experimento-uno's requirements

--- src/App.java
+++ src/App.java
@@ -8,24 +8,14 @@ public class App {
 
         System.out.println("\n╔════════════════════════════════════════════════╗");
         System.out.println("║  🍌 Welcome to the Monkey Park Simulator! 🙊   ║");
-        System.out.println("╚════════════════════════════════════════════════╝\n" + //
+        System.out.println("╚════════════════════════════════════════════════╝\n" +
                 "");
 
-        Tourist touristOne = new Tourist(1, "(👩) Alice", 2, 2, monkeyPark);
-        Tourist touristTwo = new Tourist(2, "(👴) Bob", 3, 2, monkeyPark);
-        Tourist touristThree = new Tourist(3, "(👶) Timmy", 8, 2, monkeyPark);
+        Tourist tourist = new Tourist(3, "(👵) Concepción", 5, 2, monkeyPark);
 
-        Monkey monkeyOne = new Monkey(1, "(🐒) George", 2, 2, monkeyPark);
-        Monkey monkeyTwo = new Monkey(2, "(🐒) Charlie", 3, 4, monkeyPark);
-        Monkey monkeyThree = new Monkey(3, "(🦍) Kong", 7, 2, monkeyPark);
-        Monkey monkeyFour = new Monkey(4, "(🐒) Pepito", 1, 10, monkeyPark);
+        Monkey monkey = new Monkey(1, "(🐒) Chemita Jr.", 5, 3, monkeyPark);
 
-        touristOne.start();
-        touristTwo.start();
-        touristThree.start();
-        monkeyOne.start();
-        monkeyTwo.start();
-        monkeyThree.start();
-        monkeyFour.start();
+        tourist.start();
+        monkey.start();
     }
 }
