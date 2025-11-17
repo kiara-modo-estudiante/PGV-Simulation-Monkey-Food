7a0ebd3e7010f5379b4babcb4059b2b4a6c5380a
Kiara Maldonado <usr4066@salesianos-lacuesta.net>
2025-11-17 12:26:48 +0000

    feat(experimentoDos): update app for experimento-dos requirements

--- src/App.java
+++ src/App.java
@@ -11,14 +11,14 @@ public class App {
         System.out.println("╚════════════════════════════════════════════════╝\n" + //
                 "");
 
-        Tourist touristOne = new Tourist(1, "(👩) Alice", 2, 2, monkeyPark);
-        Tourist touristTwo = new Tourist(2, "(👴) Bob", 3, 2, monkeyPark);
-        Tourist touristThree = new Tourist(3, "(👶) Timmy", 8, 2, monkeyPark);
+        Tourist touristOne = new Tourist(1, "(👩) Alice", 2, 1, monkeyPark);
+        Tourist touristTwo = new Tourist(2, "(👴) Bob", 3, 1, monkeyPark);
+        Tourist touristThree = new Tourist(3, "(👶) Timmy", 4, 1, monkeyPark);
 
-        Monkey monkeyOne = new Monkey(1, "(🐒) George", 2, 2, monkeyPark);
-        Monkey monkeyTwo = new Monkey(2, "(🐒) Charlie", 3, 4, monkeyPark);
-        Monkey monkeyThree = new Monkey(3, "(🦍) Kong", 7, 2, monkeyPark);
-        Monkey monkeyFour = new Monkey(4, "(🐒) Pepito", 1, 10, monkeyPark);
+        Monkey monkeyOne = new Monkey(1, "(🐒) George", 2, 16, monkeyPark);
+        Monkey monkeyTwo = new Monkey(2, "(🐒) Charlie", 3, 14, monkeyPark);
+        Monkey monkeyThree = new Monkey(3, "(🦍) Kong", 3, 18, monkeyPark);
+        Monkey monkeyFour = new Monkey(4, "(🐒) Pepito", 1, 30, monkeyPark);
 
         touristOne.start();
         touristTwo.start();
